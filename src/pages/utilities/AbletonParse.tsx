6a02a796f114c016f78e1a52fdbb8bed467f1a60
Mikkel RINGAUD <vexitofficial@gmail.com>
2021-10-28 16:36:32 +0200

    More informations on the parsed Ableton project

--- src/pages/utilities/AbletonParse.tsx
+++ src/pages/utilities/AbletonParse.tsx
@@ -1,31 +1,49 @@
 import { useState } from "react";
 import pako from "pako";
 
+import parseAbletonData, { CleanedAbletonData } from "../../utils/parseAbletonData";
+
 export default function UtilitiesAbletonParse () {
-  const [parsedAbleton, setParsedAbleton] = useState("");
+  const [cleanedData, setCleanedData] = useState<CleanedAbletonData>();
+  const [isError, setIsError] = useState(false);
 
   const alsImportHandler = (evt: React.ChangeEvent<HTMLInputElement>) => {
     evt.preventDefault();
 
-    if (evt.target.files && evt.target.files.length > 0) {
-      const file = evt.target.files[0];
+    // Check if a file has been uploaded.
+    const files = evt.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
 
+      // Read the ".als" file as a binary string.
       const reader = new FileReader();
       reader.onload = () => {
         const buffer = reader.result as ArrayBuffer;
-        console.log(buffer);
         const array = new Uint8Array(buffer);
 
+        // Decompress the binary string into a string.
         const ungziped = pako.ungzip(array);
-        const alsString = new TextDecoder("utf-8").decode(ungziped);
+        const xmlString = new TextDecoder("utf-8").decode(ungziped);
 
-        setParsedAbleton(alsString);
+        // Parse the string into a XML object.
+        const xmlParser = new DOMParser();
+        const xmlParsed = xmlParser.parseFromString(xmlString, "text/xml");
+
+        // Save the parsed Ableton file in state.
+        const cleanedData = parseAbletonData(xmlParsed);
+        setCleanedData(cleanedData);
+
+        console.log(cleanedData);
+
+        // Reset every other state.
+        setIsError(false);
       }
 
+      // Load the file.
       reader.readAsArrayBuffer(file);
     }
     else {
-      console.error('No file selected');
+      setIsError(true);
     }
   };
 
@@ -34,17 +52,32 @@ export default function UtilitiesAbletonParse () {
       <h1>Ableton Parse</h1>
       <p>
         Give a .als file, and you should be able to get an XML response
-        in the console. This is useful for debugging.
+        in the console.
       </p>
 
       <input
         type="file"
+        accept=".als"
+        multiple={false}
         onChange={alsImportHandler}
       />
 
-      <pre>
-        {parsedAbleton}
-      </pre>
+      {cleanedData
+        ? <div>
+            <h2>Loaded !</h2>
+            <p>
+              <strong>Ableton Used: </strong> {cleanedData.abletonVersion}
+            </p>
+          </div>
+        : <h2>Not loaded.</h2>
+      }
+
+      {isError
+        && <p>
+          Sorry, but something went wrong. Please try again.
+          Don't forget to select a <strong>.als</strong> file.
+        </p>
+      }
     </div>
   );
 }
---
+++ src/utils/parseAbletonData.ts
@@ -0,0 +1,87 @@
+export type CleanedAbletonData = {
+  abletonVersion: string;
+  tracksData: TrackData[];
+};
+
+export default function parseAbletonData (
+  project: Document
+): CleanedAbletonData {
+
+  // Short-hands
+  const notFound = "Not found !";
+  const getFirstElementByTag = (tag: string) => {
+    const element = project.getElementsByTagName(tag)[0];
+    if (!element) {
+      throw new Error(`${tag} not found !`);
+    }
+
+    return element;
+  };
+
+  // Get Ableton version (ex.: "Ableton Live 11.0.11").
+  const abletonVersion = getFirstElementByTag("Ableton").getAttribute("Creator") ?? notFound;
+
+  // Get tracks data (in JSON).
+  const tracksData = getTracksData(getFirstElementByTag("Tracks"));
+
+  return {
+    abletonVersion,
+    tracksData
+  };
+}
+
+type TrackData = {
+  type: "midi" | "audio";
+  name: string;
+}
+
+/**
+ * Remind: we only care about Audio and Midi tracks.
+ * We only care about the tracks that are used in a Launchpad project.
+ * (so we don't need to check EVERY values :D)
+ */
+function getTracksData (tracks: Element) {
+  const tracksData: TrackData[] = [];  // Initialize the output array.
+
+  const getTrackName = (track: Element) => {
+    return track.getElementsByTagName("EffectiveName")[0].getAttribute("Value") as string;
+  }
+  
+  for (const track of tracks.children) {
+    // We check the track type as we
+    // only check Audio and Midi tracks.
+    const trackType = track.tagName;
+    if (trackType === "AudioTrack" || trackType === "MidiTrack") {
+      // There's nothing much interesting in AudioTracks,
+      // what's interesting is looking if there's samples reference.
+      if (trackType === "AudioTrack") {
+        tracksData.push({
+          type: "audio",
+          name: getTrackName(track)
+        });
+      }
+
+      // Most interesting is MidiTracks.
+      // Here, there's all the logic behind a Launchpad project.
+      // As of Instrument Racks, Drum Racks, Plug-ins, ...
+      else if (trackType === "MidiTrack") {
+
+        // Get `DeviceChain` element => Contains the list of devices in the track.
+        const deviceChainGlobal = track.getElementsByTagName("DeviceChain")[0];
+        // Get `DeviceChain` in the `DeviceChain` element (yes).
+        const deviceChain = deviceChainGlobal.getElementsByTagName("DeviceChain")[0];
+        const devices = deviceChain.getElementsByTagName("Devices")[0];
+
+        //
+        console.log(devices);
+
+        tracksData.push({
+          type: "midi",
+          name: getTrackName(track)
+        });
+      }
+    }
+  }
+
+  return tracksData;
+}
