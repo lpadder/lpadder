3b7608c8e23cb9c451cd2898249299d0ea253055
Mikkel RINGAUD <vexitofficial@gmail.com>
2021-11-22 12:09:15 +0000

    Trying to improve home page ! (+ fixed colors in backgrounds !)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,60 @@
 import { Link } from "react-router-dom";
 import LpadderLogo from "@/assets/icon.png";
 
+const SubMenu = ({ title, description, to }) => {
+  return (
+    <Link to={to} className="flex-1">
+      <div className="p-4 bg-blue-800 bg-opacity-40 rounded-md backdrop-filter backdrop-blur">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </Link>
+  );
+};
+
+const LinkFooter = ({ content, link }) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer noopener"
+      className=""
+    >
+      {content}
+    </a>
+  );
+}
+
 export default function Home () {
   return (
     <div className="
       bg-no-repeat bg-contain w-screen h-screen
       bg-home-mobile bg-bottom
       sm:bg-home-desktop sm:bg-right
+      p-8
     ">
 
-      <nav className="bg-transparent w-screen h-36 flex flex-row sm:justify-start justify-center">
-        <div className="p-8">
-          <img className="h-full w-auto" alt="lpadder's logo" src={LpadderLogo} />
-        </div>
+      <nav className="bg-transparent w-full h-36 flex flex-row sm:justify-start justify-center">
+        <img className="h-24 w-24" alt="lpadder's logo" src={LpadderLogo} />
       </nav>
 
-      <div>
-        <h2>Open a lpadder project</h2>
+      <div className="flex flex-col flex-wrap items-center sm:items-start gap-4">
+        <SubMenu
+          title="Projects"
+          description="Take a look to your saved projects !"
+          to="/projects"
+        />
+
+        <SubMenu
+          title="Utilities"
+          description="Want to do some crazy stuff ?"
+          to="/utilities"
+        />
       </div>
 
-      <div>
-        <Link to="/utilities">
-          <h2>Utilities</h2>
-        </Link>
-        <p>
-          Utilities like an Ableton project parser.
-        </p>
-      </div>
+      <footer className="absolute bottom-0 left-0 sm:left-auto sm:w-auto w-full h-24 flex items-center sm:justify-start justify-center">
+        <p>Made with ❤️ by <LinkFooter content="Vexcited" link="https://github.com/Vexcited" />.</p>
+      </footer>
     </div>
   )
 }
