88421b8f29ba01483451e40cb84aee460cd221b1
Mikkel RINGAUD <vexitofficial@gmail.com>
2022-01-09 18:43:02 +0100

    Code-splitting on Utilities (reduce bundle size)

--- src/pages/utilities/index.jsx
+++ src/pages/utilities/index.jsx
@@ -1,35 +1,42 @@
 import { Link, Routes, Route } from "react-router-dom";
-import { Fragment } from "react";
+import { lazy, Suspense } from "react";
 
 // Utilities Routes.
-import AbletonParse from "./ableton-parse";
-import ConvertMidi from "./convert-midi";
+const AbletonParse = lazy(() => import("./ableton-parse"));
+const ConvertMidi = lazy(() => import("./convert-midi"));
 
+// List of all utilities displayed on 404 utility or Home.
+const UtilitiesHome = () => {
+  return (
+    <div>
+      <h1>lpadder's Utilities</h1>
+      <p>Here you'll find a lot of utilities around the Launchpad.</p>
+
+      <ul>
+        <li>
+          <Link to="ableton-parse">
+            Parse Ableton Live Set (.als) file
+          </Link>
+        </li>
+        <li>
+          <Link to="convert-midi">
+            Convert your MIDI files to Launchpad's programmer or live layout.
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// Routes for `/utilities`.
 export default function Utilities () {
   return (
-    <Fragment>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="ableton-parse" element={<AbletonParse />} />
         <Route path="convert-midi" element={<ConvertMidi />} />
+        <Route path="*" element={<UtilitiesHome />} />
       </Routes>
-
-      <div>
-        <h1>lpadder's Utilities</h1>
-        <p>Here you'll find a lot of utilities around the Launchpad.</p>
-
-        <ul>
-          <li>
-            <Link to="ableton-parse">
-              Parse Ableton Live Set (.als) file
-            </Link>
-          </li>
-          <li>
-            <Link to="convert-midi">
-              Convert your MIDI files to Launchpad's programmer or live layout.
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </Fragment>
-  )
+    </Suspense>
+  );
 }
