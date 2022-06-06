2415d16f9476d9daf33f0a7474d166fbb55473f0
Mikkel RINGAUD <mikkel@milescode.dev>
2022-06-07 01:53:40 +0200

    refactor: `Modal` and `DropdownButton` to SolidJS

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -1,72 +1,111 @@
-import React, { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import { Component, JSX, Match, Switch } from "solid-js";
+import { Menu, Transition, MenuItem, Popover, PopoverButton, PopoverPanel } from "solid-headless";
+import { For } from "solid-js";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
-type DropdownButtonProps = {
-  children: React.ReactNode;
-  items: { name: string; action: () => void; }[];
-  buttonClassName: string;
-  alwaysShow?: boolean;
-};
-
-export default function DropdownButton ({
-  items,
-  children,
-  buttonClassName,
-  alwaysShow = false
-}: DropdownButtonProps) {
+function Separator() {
   return (
-    <Menu
-      as="div"
-      onClick={
-        /** We stop propagation to prevent click event on parents. */
-        (e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()
-      }
-      className="inline-block relative text-left"
-    >
-      {!alwaysShow &&
-        <div>
-          <Menu.Button className={buttonClassName}>
-            {children}
-          </Menu.Button>
-        </div>
-      }
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items
-          static={alwaysShow}
-          className={"z-50 absolute right-0 mt-2 w-56 bg-gray-800 rounded-md shadow-md origin-top-right"}
-        >
-          <div className="py-1">
-            {items.map(item =>
-              <Menu.Item key={item.name}>
-                {({ active }) => (
-                  <a
-                    onClick={item.action}
-                    className={classNames(
-                      active ? "bg-gray-600 bg-opacity-60" : "bg-gray-600 bg-opacity-40",
-                      "block px-2 py-2 mx-2 my-1 text-sm rounded transition-colors cursor-pointer"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                )}
-              </Menu.Item>
-            )}
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+    <div class="flex items-center" aria-hidden="true">
+      <div class="w-full border-t border-gray-200" />
+    </div>
   );
 }
+
+const DropdownButton: Component<{
+  items: { name?: string, action?: () => unknown, isSeparator?: boolean }[],
+  buttonIcon: JSX.Element,
+  buttonClassName: string
+}> = (props) => {
+  return (
+    <Popover class="relative">
+      {({ isOpen }) => (
+        <>
+          <PopoverButton
+            class={props.buttonClassName}
+          >
+            {props.buttonIcon}
+          </PopoverButton>
+          <Transition
+            show={isOpen()}
+            enter="transition duration-200"
+            enterFrom="opacity-0 -translate-y-1 scale-50"
+            enterTo="opacity-100 translate-y-0 scale-100"
+            leave="transition duration-150"
+            leaveFrom="opacity-100 translate-y-0 scale-100"
+            leaveTo="opacity-0 -translate-y-1 scale-50"
+          >
+            <PopoverPanel unmount={false} class="absolute z-10 px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl">
+              <Menu class="z-50 absolute right-0 mt-2 w-56 bg-gray-800 rounded-md shadow-md origin-top-right">
+                <For each={props.items}>
+                  {item => (
+                    <Switch>
+                      <Match when={!item.isSeparator}>
+                        <MenuItem
+                          as="button"
+                          onClick={item.action}
+                          class="hover:bg-gray-600 hover:bg-opacity-60 bg-gray-600 bg-opacity-40 block px-2 py-2 mx-2 my-1 text-sm rounded transition-colors cursor-pointer"
+                        >
+                          {item.name}
+                        </MenuItem>
+                      </Match>
+                      <Match when={item.isSeparator}>
+                        <Separator />
+                      </Match>
+                    </Switch>
+                  )}
+                </For>
+              </Menu>
+            </PopoverPanel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+    // <Menu
+    //   as="div"
+    //   onClick={
+    //     /** We stop propagation to prevent click event on parents. */
+    //     (e) => e.stopPropagation()
+    //   }
+    //   class="inline-block relative text-left"
+    // >
+    //   {<Show when={!props.alwaysShow}>{<div>
+    //     <Menu.Button class={props.buttonClassName}>
+    //       {props.children}
+    //     </Menu.Button>
+    //   </div>}</Show>
+    //   }
+
+  //   <Transition
+  //     show={isOpen()}
+  //     enter="transition duration-200"
+  //     enterFrom="opacity-0 -translate-y-1 scale-50"
+  //     enterTo="opacity-100 translate-y-0 scale-100"
+  //     leave="transition duration-150"
+  //     leaveFrom="opacity-100 translate-y-0 scale-100"
+  //     leaveTo="opacity-0 -translate-y-1 scale-50"
+  //   >
+  //     <Menu
+  //       class={"z-50 absolute right-0 mt-2 w-56 bg-gray-800 rounded-md shadow-md origin-top-right"}
+  //     >
+  //       <div class="py-1">
+  //         <For each={props.items}>{item =>
+  //           <MenuItem key={item.name}>
+  //             {({ active }) => (
+  //               <a
+  //                 onClick={item.action}
+  //                 class={classNames(
+  //                   active ? "bg-gray-600 bg-opacity-60" : "bg-gray-600 bg-opacity-40",
+  //                   "block px-2 py-2 mx-2 my-1 text-sm rounded transition-colors cursor-pointer"
+  //                 )}
+  //               >
+  //                 {item.name}
+  //               </a>
+  //             )}
+  //           </MenuItem>}</For>
+  //       </div>
+  //     </Menu>
+  //   </Transition>
+  // </Menu>
+  );
+};
+
+export default DropdownButton;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,47 +1,59 @@
 import type { ParentComponent } from "solid-js";
 
-import { onCleanup, onMount, Show, children } from "solid-js";
 import { Portal } from "solid-js/web";
+
+import {
+  Transition,
+  TransitionChild,
+  Dialog,
+  DialogOverlay,
+  DialogPanel
+} from "solid-headless";
 
 const Modal: ParentComponent<{
   open: boolean;
   onClose: () => void;
 }> = (props) => {
-  let backdrop: HTMLDivElement | undefined;
-
-  /** Close modal on `Escape` key. */
-  const keyHandler = (e: KeyboardEvent) => e.code === "Escape" && props.onClose();
-
-  /** Close modal on backdrop element click. */
-  const clickHandler = (e: MouseEvent) => e.target === backdrop && props.onClose();
-
-  onMount(() => {
-    if (!backdrop) return;
-
-    backdrop.addEventListener("click", clickHandler);
-    window.addEventListener("keyup", keyHandler);
-  });
-
-  onCleanup(() => {
-    if (!backdrop) return;
-
-    backdrop.removeEventListener("click", clickHandler);
-    window.removeEventListener("keyup", keyHandler);
-  });
-
   return (
-    <Show when={props.open}>
-      <Portal>
-        <div
-          ref={backdrop}
-          class={`${props.open && "flex fixed top-0 left-0 z-50 justify-center items-center w-screen h-screen bg-gray-900 bg-opacity-60"}`}
+    <Portal>
+      <Transition
+        appear
+        show={props.open}
+      >
+        <Dialog
+          isOpen
+          class="fixed inset-0 overflow-y-auto z-40"
+          onClose={() => props.onClose()}
         >
-          <div class="p-4 mx-4 w-full max-w-md bg-gray-800 rounded-lg border-2 border-gray-900 shadow-lg shadow-gray-900">
-            {children(() => props.children)()}
+          <div class="min-h-screen px-4 flex items-center justify-center">
+            <TransitionChild
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <DialogOverlay class="fixed inset-0 bg-gray-900 bg-opacity-50" />
+            </TransitionChild>
+
+            <TransitionChild
+              class="z-50"
+              enter="ease-out duration-200"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <DialogPanel class=" p-4 mx-4 w-full max-w-md bg-gray-800 rounded-lg border-2 border-gray-900 shadow-lg shadow-gray-900">
+                {props.children}
+              </DialogPanel>
+            </TransitionChild>
           </div>
-        </div>
-      </Portal>
-    </Show>
+        </Dialog>
+      </Transition>
+    </Portal>
   );
 };
 
