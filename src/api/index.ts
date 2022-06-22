ede5c57b732cd9f3ab79a0c269dd71323db873fb
Mikkel RINGAUD <mikkel@milescode.dev>
2022-06-22 17:14:01 +0000

    refactor: use `solid-start` API routes

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Handler } from "vite-plugin-mix"
-import send from "@polka/send-type";
-
-export const handler: Handler = (req, res, next) => {
-  if (!req.path.startsWith("/api")) return next();
-
-  send(res, 200, { hello: "world" });
-};
