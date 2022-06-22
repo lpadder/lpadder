618900ee6fbd9e9195736862c29fdaa1d09c43b7
Mikkel RINGAUD <mikkel@milescode.dev>
2022-06-22 16:01:31 +0000

    chore: use WindiCSS

---
+++ windi.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "windicss/helpers";
+import colors from "windicss/colors";
+
+import aspectRatioPlugin from "windicss/plugin/aspect-ratio";
+
+export default defineConfig({
+  darkMode: "class",
+
+  theme: {
+    extend: {
+      backgroundImage: {
+        "home-desktop": "url('/src/assets/backgrounds/home-desktop.svg')",
+        "home-mobile": "url('/src/assets/backgrounds/home-mobile.svg')"
+      }
+    },
+
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      pink: colors.fuchsia,
+      gray: colors.slate,
+      blue: colors.sky
+    },
+    
+    fontFamily: {
+      sans: ["Poppins", "sans-serif "]
+    }
+  },
+
+  plugins: [aspectRatioPlugin]
+});
