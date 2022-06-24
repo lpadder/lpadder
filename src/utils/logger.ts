9b48d50c1727f9a2af2538b53287311ef98e9570
Mikkel RINGAUD <mikkel@milescode.dev>
2022-06-24 10:27:51 +0200

    feat: new logger for better logs

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+const wrapLabel = (label: string) => `[${label}]`;
+
+export const logStart = (label: string, info: string) => {
+  log(label, info);
+  console.time(wrapLabel(label));
+};
+
+export const logEnd = (label: string) => {
+  console.timeEnd(wrapLabel(label));
+};
+
+export const log = (label: string, ...message: unknown[]) => {
+  console.info(wrapLabel(label) + ":", ...message);
+};
+  
