494dc0bb5163073fd2301c6903b9d43bd7264ddb
trobonox <57040351+trobonox@users.noreply.github.com>
2022-07-03 19:10:23 +0200

    docs: add basic docusaurus files with config and deps

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,90 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'lpadder.',
+  tagline: 'With lpadder you do not need to pay $800 for Live Suite, you have everything in your browser, for FREE!',
+  url: 'https://docs.lpadder.ml',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Vexcited', // Usually your GitHub org/user name.
+  projectName: 'lpadder', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      title: "lpadder",
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
+      navbar: {
+        title: 'lpadder',
+        logo: {
+          alt: 'Lpadder Logo',
+          src: 'img/lpadder_icon.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://github.com/vexcited/lpadder',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        links: [
+          { label: 'github', to: 'https://github.com/vexcited/lpadder' },
+          { label: 'discord', to: 'https://discord.gg/aoijdaoidj' }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Vexcited & Trobonox.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
