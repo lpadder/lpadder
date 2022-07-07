8ff7b61a55ab348d24aa6bf72e58bd5924defb69
Mikkel RINGAUD <mikkel@milescode.dev>
2022-07-07 22:52:06 +0200

    styles: add `forceOpenToRight` prop to dropdown

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -1,32 +1,40 @@
 import type { Component, JSX } from "solid-js";
 import { Menu, Transition, MenuItem, Popover, PopoverButton, PopoverPanel } from "solid-headless";
+import { classNames } from "@/utils/styling";
 
 const DropdownButton: Component<{
   items: { name?: string, action?: () => unknown }[][],
   buttonIcon: JSX.Element,
-  buttonClassName: string
+  buttonClassName?: string,
+  /** Force the dropdown to `translate-x` itself. */
+  forceOpenToRight?: boolean
 }> = (props) => {
   return (
-    <Popover class="relative">
-      {({ isOpen }) => (
+    <Popover class="relative z-99">
+      {({ isOpen, setState }) => (
         <>
-          <PopoverButton
-            class={`flex ${props.buttonClassName}`}
-          >
+          <PopoverButton class={classNames("flex", props.buttonClassName)}>
             {props.buttonIcon}
           </PopoverButton>
 
           <Transition
             show={isOpen()}
-            enter="transition duration-100"
-            enterFrom="opacity-0 -translate-y-1 scale-50"
-            enterTo="opacity-100 translate-y-0 scale-100"
-            leave="transition duration-150"
-            leaveFrom="opacity-100 translate-y-0 scale-100"
-            leaveTo="opacity-0 -translate-y-1 scale-50"
+            class="transform"
+            enter="ease-out duration-200"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-100"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-85"
           >
-            <PopoverPanel unmount={false} class="absolute z-20 p-2 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl">
-              <Menu class="flex flex-col gap-1 absolute right-0 w-48 origin-top-right">
+            <PopoverPanel
+              unmount={false}
+              class="absolute p-2 sm:px-0 lg:max-w-3xl"
+              classList={{
+                "transform translate-x-48": props.forceOpenToRight
+              }}
+            >
+              <Menu class={classNames("transition flex flex-col gap-1 absolute right-0 w-48", props.forceOpenToRight ? "origin-top-left" : "origin-top-right")}>
                 <For each={props.items}>
                   {group_items => (
                     <div class="rounded-md backdrop-filter backdrop-blur bg-gray-800 bg-opacity-80 border border-gray-900 p-1.5">
@@ -34,7 +42,10 @@ const DropdownButton: Component<{
                         {item => (
                           <MenuItem
                             as="button"
-                            onClick={item.action}
+                            onClick={() => {
+                              setState(false); // Closes the dropdown.
+                              if (item.action) item.action();
+                            }}
                             class="hover:(bg-gray-600 bg-opacity-100) bg-gray-400 bg-opacity-60 w-full block px-2 py-2 text-sm rounded transition-colors cursor-pointer mb-1 last:mb-0"
                           >
                             {item.name}
