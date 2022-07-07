172471cc3491449558b4a9bf818f87d2d01d1a9b
Mikkel RINGAUD <mikkel@milescode.dev>
2022-07-07 10:07:41 +0200

    feat: support launchpad mk2 component

---
+++ src/components/devices/LaunchpadMK2.tsx
@@ -0,0 +1,40 @@
+import type { DeviceComponent } from "@/components/devices";
+import { DeviceButton } from "@/components/devices";
+
+import { devicesConfiguration } from "@/utils/devices";
+import { classNames } from "@/utils/styling";
+
+const LaunchpadMK2: DeviceComponent = (props) => {
+  const configuration = devicesConfiguration["launchpad_mk2"];
+
+  return (
+    <div class="flex flex-col gap-1" ref={props.ref}>
+      <For each={configuration.layout_to_use}>
+        {(rows, rowIndex) => (
+          <div class="flex flex-row gap-1">
+            <For each={rows}>
+              {(padId) => (padId !== -1)
+                ? <DeviceButton
+                  class={classNames(
+                    (
+                      rowIndex() === 0
+                      || padId.toString()[1] === "9"
+                    )
+                      ? "__phantom-pad rounded-full after:rounded-full" : "rounded-sm",
+                    "bg-gray-400 h-full w-full aspect-square"
+                  )}
+                  id={padId}
+                  context={props.onContext}
+                  up={props.onPadUp}
+                  down={props.onPadDown}
+                /> : <div class="h-full w-full" />
+              }
+            </For>
+          </div>
+        )}
+      </For>
+    </div>
+  );
+};
+
+export default LaunchpadMK2;
