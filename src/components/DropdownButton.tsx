32787fde5de0c25bc959297fa942db781f505e46
Mikkel RINGAUD <mikkel@milescode.dev>
2022-07-10 23:44:04 +0200

    styles(DropdownButton): fix useless gap when we have 0 items

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -19,24 +19,23 @@ const DropdownButton: Component<{
 
           <Transition
             show={isOpen()}
-            class="transform"
-            enter="ease-out duration-200"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-100"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-85"
+            enter="transform transition duration-200"
+            enterFrom="transform opacity-0 -translate-y-1 scale-50"
+            enterTo="transform opacity-100 translate-y-0 scale-100"
+            leave="transform transition duration-150"
+            leaveFrom="transform opacity-100 translate-y-0 scale-100"
+            leaveTo="transform opacity-0 -translate-y-1 scale-50"
           >
             <PopoverPanel
               unmount={false}
-              class="absolute p-2 sm:px-0 lg:max-w-3xl"
+              class="absolute z-10 px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl"
               classList={{
                 "transform translate-x-48": props.forceOpenToRight
               }}
             >
               <Menu class={classNames("transition flex flex-col gap-1 absolute right-0 w-48", props.forceOpenToRight ? "origin-top-left" : "origin-top-right")}>
                 <For each={props.items}>
-                  {group_items => (
+                  {group_items => group_items.length > 0 && (
                     <div class="rounded-md backdrop-filter backdrop-blur bg-gray-800 bg-opacity-80 border border-gray-900 p-1.5">
                       <For each={group_items}>
                         {item => (
