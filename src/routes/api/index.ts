b2f339b6f451342c6942fcd86a39ba9e2e1ef7ab
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-06 18:22:05 +0200

    refactor: remove API routes temporary

--- src/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import type { ApiRequestHandlerType } from "@/utils/apiRequestHandler";
-import { handleApiRequest } from "@/utils/apiRequestHandler";
-
-export const get: ApiRequestHandlerType = (props) => handleApiRequest(props, async (req, res) => {
-  return res.json({
-    success: true,
-    message: "Something amazing is coming here, soon !"
-  });
-});
