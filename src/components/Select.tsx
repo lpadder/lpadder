edf9f067c94ca85e4f2ee3aaa38b506b287a9c0e
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-24 21:29:34 +0200

    fix(ui): props spread order with custom class spread prop

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,15 +3,17 @@ import type { ParentComponent, JSX } from "solid-js";
 const Select: ParentComponent<JSX.IntrinsicElements["select"]> = (props) => {
   return (
     <select
-      class="
+      {...props}
+      class={`
         py-2 px-4 w-full rounded-lg outline-none
         bg-slate-900 bg-opacity-40
         transition-colors focus:bg-opacity-100
         text-slate-400 appearance-none
 
         border border-slate-900 hover:bg-opacity-60 focus:border-slate-400
-      "
-      {...props}
+
+        ${props.class ?? ""}
+      `}
     />
   );
 };
